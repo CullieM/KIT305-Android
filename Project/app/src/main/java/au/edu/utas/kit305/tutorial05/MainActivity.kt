f4c0a56f57854bd65419a583d254dcd87c84285e
Cullie <cullie.02@gmail.com>
2021-04-19 20:40:09 +1000

    Tutorial 5 Base Code

---
+++ Project/app/src/main/java/au/edu/utas/kit305/tutorial05/MainActivity.kt
@@ -0,0 +1,108 @@
+package au.edu.utas.kit305.tutorial05
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import au.edu.utas.kit305.tutorial05.databinding.ActivityMainBinding
+import au.edu.utas.kit305.tutorial05.databinding.MyListItemBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+
+const val FIREBASE_TAG = "FirebaseLogging"
+val items = mutableListOf<Movie>()
+const val MOVIE_INDEX = "Movie_Index"
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onResume() {
+        super.onResume()
+
+        ui.myList.adapter?.notifyDataSetChanged()
+    }
+    private lateinit var ui : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ui = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+
+        ui.lblMovieCount.text = "${items.size} Movies"
+        ui.myList.adapter = MovieAdapter(movies = items)
+
+        //vertical list
+        ui.myList.layoutManager = LinearLayoutManager(this)
+        val db = Firebase.firestore
+
+        /*
+        val lotr = Movie(
+            title = "Lord of the Rings: Fellowship of the Ring",
+            year = 2001,
+            duration = 9001F
+        )
+
+        moviesCollection
+            .add(lotr)
+            .addOnSuccessListener {
+                Log.d(FIREBASE_TAG, "Document created with id ${it.id}")
+                lotr.id = it.id
+            }
+            .addOnFailureListener {
+                Log.e(FIREBASE_TAG, "Error writing document", it)
+            }
+
+         */
+        //get all movies
+        var moviesCollection = db.collection("movies")
+
+        ui.lblMovieCount.text = "Loading..."
+        moviesCollection
+            .get()
+            .addOnSuccessListener { result ->
+                Log.d(FIREBASE_TAG, "--- all movies ---")
+                for (document in result)
+                {
+                    //Log.d(FIREBASE_TAG, document.toString())
+                    val movie = document.toObject<Movie>()
+                    movie.id = document.id
+                    Log.d(FIREBASE_TAG, movie.toString())
+
+                    items.add(movie)
+                    (ui.myList.adapter as MovieAdapter).notifyDataSetChanged()
+                }
+                ui.lblMovieCount.text = "${items.size} Movies"
+            }
+    }
+
+
+    inner class MovieHolder(var ui: MyListItemBinding) : RecyclerView.ViewHolder(ui.root) {}
+
+    inner class MovieAdapter(private val movies: MutableList<Movie>) : RecyclerView.Adapter<MovieHolder>()
+    {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainActivity.MovieHolder {
+            val ui = MyListItemBinding.inflate(layoutInflater, parent, false)   //inflate a new row from the my_list_item.xml
+            return MovieHolder(ui)                                                            //wrap it in a ViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return movies.size
+        }
+
+        override fun onBindViewHolder(holder: MainActivity.MovieHolder, position: Int) {
+            val movie = movies[position]   //get the data at the requested position
+            holder.ui.txtName.text = movie.title
+            holder.ui.txtYear.text = movie.year.toString()
+
+            holder.ui.root.setOnClickListener {
+                var i = Intent(holder.ui.root.context, MovieDetails::class.java)
+                i.putExtra(MOVIE_INDEX, position)
+                startActivity(i)
+            }
+        }
+    }
+}
+
